5ba1de9d793507d401529200a3e07acc6f109994
Dermot Sheerin <d.sheerin@hotmail.com>
2020-02-24 14:05:11 +0000

    implemented settings and update settings feature

--- app/controllers/accounts.js
+++ app/controllers/accounts.js
@@ -1,80 +1,110 @@
-'use strict';
-const User = require('../models/user');
-const Boom = require('@hapi/boom');
+"use strict";
+const User = require("../models/user");
+const Boom = require("@hapi/boom");
+const Joi = require("@hapi/joi");
 
 const Accounts = {
-    // home page (index)
-    index: {
-        auth: false,
-        handler: function(request, h) {
-            return h.view('main', { title: 'POI - islands' }); // Changes the tab at top of the web page
-        }
-    },
-
-    showSignup: {
-        auth: false,
-        handler: function(request, h) {
-            return h.view('signup', { title: 'Sign up for POI - islands' });
-        } // we can only pass in values/variables from a "return h.view" method, not a redirect
-    },
-
-    signup: {
-        auth: false,
-        handler: async function(request, h) {
-            try {
-                const payload = request.payload;
-                let user = await User.findByEmail(payload.email);
-                if (user) {
-                    const message = 'That Email address already exists!';
-                    throw Boom.badData(message);
-                }
-                const newUser = new User({
-                    firstName: payload.firstName,
-                    lastName: payload.lastName,
-                    email: payload.email,
-                    password: payload.password,
-                    userRole: 'member'
-                });
-                user = await newUser.save();
-                request.cookieAuth.set({ id: user.id });    // This is how we can set a session cookie
-                return h.redirect('/dashboard');
-            } catch (err) {
-                return h.view('signup', { errors: [{ message: err.message }] });
-            }
-        }
-    },
-
-
-    login: {
-        auth: false,
-        handler: async function(request, h) {
-            const { email, password } = request.payload;
-            try {
-                let user = await User.findByEmail(email);
-                if (!user) {
-                    const message = 'That Email address is NOT registered';
-                    throw Boom.unauthorized(message);
-                }
-                user.comparePassword(password);
-                request.cookieAuth.set({ id: user.id });
-                if (user.userRole == 'admin')
-                    return h.redirect('/adminDashboard');
-                else
-                    return h.redirect('/dashboard');
-            } catch (err) {
-                return h.view('main', { errors: [{ message: err.message }] });
-            }
-        }
-    },
-
-    logout: {
-        auth: false,
-        handler: function(request, h) {
-            request.cookieAuth.clear(); // we can clear the session
-            return h.redirect('/');
-        }
+  // home page (index)
+  index: {
+    auth: false,
+    handler: function(request, h) {
+      return h.view("main", { title: "POI - islands" }); // Changes the tab at top of the web page
     }
+  },
 
+  showSignup: {
+    auth: false,
+    handler: function(request, h) {
+      return h.view("signup", { title: "Sign up for POI - islands" });
+    } // we can only pass in values/variables from a "return h.view" method, not a redirect
+  },
+
+  signup: {
+    auth: false,
+    handler: async function(request, h) {
+      try {
+        const payload = request.payload;
+        let user = await User.findByEmail(payload.email);
+        if (user) {
+          const message = "That Email address already exists!";
+          throw Boom.badData(message);
+        }
+        const newUser = new User({
+          firstName: payload.firstName,
+          lastName: payload.lastName,
+          email: payload.email,
+          password: payload.password,
+          userRole: "member"
+        });
+        user = await newUser.save();
+        request.cookieAuth.set({ id: user.id }); // This is how we can set a session cookie
+        return h.redirect("/dashboard");
+      } catch (err) {
+        return h.view("signup", { errors: [{ message: err.message }] });
+      }
+    }
+  },
+
+  login: {
+    auth: false,
+    handler: async function(request, h) {
+      const { email, password } = request.payload;
+      try {
+        let user = await User.findByEmail(email);
+        if (!user) {
+          const message = "That Email address is NOT registered";
+          throw Boom.unauthorized(message);
+        }
+        user.comparePassword(password);
+        request.cookieAuth.set({ id: user.id });
+        if (user.userRole == "admin") return h.redirect("/adminDashboard");
+        else return h.redirect("/dashboard");
+      } catch (err) {
+        return h.view("main", { errors: [{ message: err.message }] });
+      }
+    }
+  },
+
+  logout: {
+    auth: false,
+    handler: function(request, h) {
+      request.cookieAuth.clear(); // we can clear the session
+      return h.redirect("/");
+    }
+  },
+
+  settings: {
+    handler: async function(request, h) {
+      try {
+        const id = request.auth.credentials.id;
+        const user = await User.findById(id).lean();
+        return h.view("settings", {
+          title: `${user.firstName}'s Settings`,
+          user: user
+        });
+      } catch (err) {
+        return h.view("main", { errors: [{ message: err.message }] });
+      }
+    }
+  },
+
+  updateSettings: {
+    handler: async function(request, h) {
+      try {
+        let updateUser = request.payload; // retrieves the updated user settings
+        const id = request.auth.credentials.id;
+        let user = await User.findById(id);
+        user.firstName = updateUser.firstName;
+        user.lastName = updateUser.lastName;
+        user.email = updateUser.email;
+        user.password = updateUser.password;
+        await user.save();
+        return h.redirect("/settings");
+      } catch (err) {
+        return h.view("settings", { errors: [{ message: err.message }] });
+      }
+    }
+  }
 };
 
 module.exports = Accounts;
--- routes.js
+++ routes.js
@@ -1,37 +1,42 @@
-'use strict';
+"use strict";
 
-const Accounts = require('./app/controllers/accounts');
-const Dashboard = require('./app/controllers/dashboard');
-const AdminDashboard = require('./app/controllers/adminDashboard');
-const Island = require('./app/controllers/islands');
+const Accounts = require("./app/controllers/accounts");
+const Dashboard = require("./app/controllers/dashboard");
+const AdminDashboard = require("./app/controllers/adminDashboard");
+const Island = require("./app/controllers/islands");
 
 module.exports = [
-    { method: 'GET', path: '/', config: Accounts.index },
-    { method: 'POST', path: '/authenticate', config: Accounts.login },
-    { method: 'GET', path: '/dashboard', config: Dashboard.dashboard },
-    { method: 'GET', path: '/adminDashboard', config: AdminDashboard.dashboard },
+  { method: "GET", path: "/", config: Accounts.index },
+  { method: "POST", path: "/authenticate", config: Accounts.login },
+  { method: "GET", path: "/dashboard", config: Dashboard.dashboard },
+  { method: "GET", path: "/adminDashboard", config: AdminDashboard.dashboard },
 
-    { method: 'POST', path: '/dashboard/addRegion', config: Island.addRegion },
-    { method: 'POST', path: '/dashboard/addIsland', config: Island.addIsland },
-   // { method: 'GET', path: '/dashboard/listIslands', config: Island.listIslands },
-    { method: 'GET', path: '/dashboard/{getIslands*}', config: Island.retrieveUserIslands },
+  { method: "POST", path: "/dashboard/addRegion", config: Island.addRegion },
+  { method: "POST", path: "/dashboard/addIsland", config: Island.addIsland },
+  // { method: 'GET', path: '/dashboard/listIslands', config: Island.listIslands },
+  {
+    method: "GET",
+    path: "/dashboard/{getIslands*}",
+    config: Island.retrieveUserIslands
+  },
 
+  { method: "GET", path: "/showSignup", config: Accounts.showSignup },
+  { method: "POST", path: "/signup", config: Accounts.signup },
+  { method: "GET", path: "/logout", config: Accounts.logout },
 
-    { method: 'GET', path: '/showSignup', config: Accounts.showSignup },
-    { method: 'POST', path: '/signup', config: Accounts.signup },
-    { method: 'GET', path: '/logout', config: Accounts.logout },
+  { method: "GET", path: "/settings", config: Accounts.settings },
+  { method: "POST", path: "/settings", config: Accounts.updateSettings },
 
-    // { method: 'GET', path: '/login', config: Accounts.showLogin },
+  // { method: 'GET', path: '/login', config: Accounts.showLogin },
 
-
-    {
-        method: 'GET',
-        path: '/{param*}',
-        handler: {
-            directory: {
-                path: './public'
-            }
-        },
-        options: { auth: false }    // disable authentication on the static route for public folder i.e., images stored there
-    }
+  {
+    method: "GET",
+    path: "/{param*}",
+    handler: {
+      directory: {
+        path: "./public"
+      }
+    },
+    options: { auth: false } // disable authentication on the static route for public folder i.e., images stored there
+  }
 ];
