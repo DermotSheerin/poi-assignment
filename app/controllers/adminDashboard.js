e1f36c5dfafdd2db81ff9b7bfa1b7fdc9f6af3d0
Dermot Sheerin <d.sheerin@hotmail.com>
2020-03-08 01:07:33 +0000

    Implemented validation and Error handling, also performed some unplanned work and fully Implemented add/delete multiple images per location

--- app/controllers/adminDashboard.js
+++ app/controllers/adminDashboard.js
@@ -1,4 +1,3 @@
-const Boom = require("@hapi/boom");
 const Island = require("../models/island");
 const Region = require("../models/region");
 const Joi = require("@hapi/joi");
@@ -8,21 +7,23 @@ const ImageStore = require("../utils/image-store");
 const AdminDashboard = {
   dashboard: {
     handler: async function(request, h) {
-      const users = await User.find({ userRole: "member" }).lean();
-      const regions = await Region.find({}).lean();
-
+      try {
+        const users = await User.find({ userRole: "member" }).lean();
+        const regions = await Region.find({}).lean();
+        return h.view("adminDashboard", {
+          title: "POI Dashboard - ADMIN",
+          users: users,
+          regions: regions
+        });
+      } catch (err) {
+        return h.view("adminDashboard", { errors: [{ message: err.message }] });
+      }
       // Unused code, keeping in case required in future
       // users.forEach(countUserIslands); // iterate through the users array and call countUserIslands function for each user
       // async function countUserIslands(user) {
       //   // pass the user to the Island model to count the number of Islands created by that user
       //   user.islandCount = await Island.countUserIslands(user._id);
       // }
-
-      return h.view("adminDashboard", {
-        title: "POI Dashboard - ADMIN",
-        users: users,
-        regions: regions
-      });
     }
   },
 
@@ -62,20 +63,28 @@ const AdminDashboard = {
 
   deleteRegion: {
     handler: async function(request, h) {
-      const regionID = request.params.id;
-      await Region.findByIdAndDelete(regionID);
-      return h.redirect("/adminDashboard");
+      try {
+        const regionID = request.params.id;
+        await Region.findByIdAndDelete(regionID);
+        return h.redirect("/adminDashboard");
+      } catch (err) {
+        return h.view("adminDashboard", { errors: [{ message: err.message }] });
+      }
     }
   },
 
   deleteMember: {
     handler: async function(request, h) {
-      const userID = request.params.id;
-      const userIslands = await Island.findIslandsByUserId(userID).lean();
-      await ImageStore.deleteUserIslandImages(userIslands); // delete users Island images from cloudinary before deleting the island and then deleting the user
-      await Island.deleteIslandsByUserId(userID); // prior to deleting member, delete all islands associate with the member
-      await User.findByIdAndDelete(userID);
-      return h.redirect("/adminDashboard");
+      try {
+        const userID = request.params.id;
+        const userIslands = await Island.findIslandsByUserId(userID).lean();
+        await ImageStore.deleteUserIslandImages(userIslands); // delete users Island images from cloudinary before deleting the island and then deleting the user
+        await Island.deleteIslandsByUserId(userID); // prior to deleting member, delete all islands associate with the member
+        await User.findByIdAndDelete(userID);
+        return h.redirect("/adminDashboard");
+      } catch (err) {
+        return h.view("adminDashboard", { errors: [{ message: err.message }] });
+      }
     }
   }
 };
